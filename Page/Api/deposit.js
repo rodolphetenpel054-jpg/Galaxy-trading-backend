572d5acbc8ce3c8c7d52c3e5ee0f70d572908148
Galaxy trading <228169516+rodolphetenpel054-jpg@users.noreply.github.com>
2026-01-05 13:43:53 +0100

    Add deposit API functionality with form elements

---
+++ Page/Api/deposit.js
@@ -0,0 +1,18 @@
+Value{amount}
+onechange-{(e)->
+  serAmount (e,target.value)}
+/>
+<br/><br/>
+  <Select value _method}
+onchange_{(e)_>
+  select method(e,target.value)}><option 
+value_mobile_money>mobile money</option>
+<option value _"usdt">usdt</option>
+<option value_"usdt">usdt</option<
+  </select>
+<br/><br/>
+  <button 
+onclicke_handler deposit}>valider le dépôt</button>
+  </div>
+);
+  }
