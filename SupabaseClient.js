fdc15e1cd178bf6ccbc280565effcb9aa8811c71
Galaxy trading <228169516+rodolphetenpel054-jpg@users.noreply.github.com>
2026-01-25 18:52:14 +0100

    Initialize Supabase client in SupabaseClient.js
    Créer un formulaire d'inscription dépôt retrait historique active page administrative

---
+++ SupabaseClient.js
@@ -0,0 +1,8 @@
+JavaScript 
+import{createClient}from@supabase/supabase
+const supabase URL-
+process.env.NEXT_PUBLIC_SUPABASE_URLse-js
+const supabaseANONKEY-
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+export const supabase-
+createClient supabaseurl,supabaseAnonKey
